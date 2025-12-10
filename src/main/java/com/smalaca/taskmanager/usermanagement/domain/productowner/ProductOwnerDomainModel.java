3f37f4f12cfcee7e1423404bc25853e160bb08f2
smalaca <sebastian.malaca@gmail.com>
2025-12-10 16:03:58 +0100

    remove field ProductOwner form ProductOwnerDomainModel

--- src/main/java/com/smalaca/taskmanager/usermanagement/domain/productowner/ProductOwnerDomainModel.java
+++ src/main/java/com/smalaca/taskmanager/usermanagement/domain/productowner/ProductOwnerDomainModel.java
@@ -3,32 +3,55 @@ package com.smalaca.taskmanager.usermanagement.domain.productowner;
 import com.smalaca.taskamanager.model.entities.ProductOwner;
 import com.smalaca.taskamanager.model.entities.Project;
 
-import static java.util.stream.Collectors.toList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class ProductOwnerDomainModel {
-    private final ProductOwner productOwner;
+    private final Long id;
+    private final String firstName;
+    private final String lastName;
+    private String phonePrefix;
+    private String phoneNumber;
+    private String emailAddress;
+    private final List<Long> projectIds;
+    private final boolean hasPhoneNumber;
+    private final boolean hasEmailAddress;
 
     ProductOwnerDomainModel(ProductOwner productOwner) {
-        this.productOwner = productOwner;
+        id = productOwner.getId();
+        firstName = productOwner.getFirstName();
+        lastName = productOwner.getLastName();
+        hasPhoneNumber = productOwner.getPhoneNumber() != null;
+        if (hasPhoneNumber) {
+            phonePrefix = productOwner.getPhoneNumber().getPrefix();
+            phoneNumber = productOwner.getPhoneNumber().getNumber();
+        }
+        hasEmailAddress = productOwner.getEmailAddress() != null;
+
+        if (hasEmailAddress) {
+            emailAddress = productOwner.getEmailAddress().getEmailAddress();
+        }
+        projectIds = productOwner.getProjects().stream().map(Project::getId).collect(Collectors.toList());
     }
 
     public ProductOwnerView asView() {
-        ProductOwnerView dto = new ProductOwnerView();
-        dto.setId(productOwner.getId());
-        dto.setFirstName(productOwner.getFirstName());
-        dto.setLastName(productOwner.getLastName());
+        ProductOwnerView view = new ProductOwnerView();
+        view.setId(id);
+        view.setFirstName(firstName);
+        view.setLastName(lastName);
 
-        if (productOwner.getPhoneNumber() != null) {
-            dto.setPhonePrefix(productOwner.getPhoneNumber().getPrefix());
-            dto.setPhoneNumber(productOwner.getPhoneNumber().getNumber());
+        if (hasPhoneNumber) {
+            view.setPhonePrefix(phonePrefix);
+            view.setPhoneNumber(phoneNumber);
         }
 
-        if (productOwner.getEmailAddress() != null) {
-            dto.setEmailAddress(productOwner.getEmailAddress().getEmailAddress());
+        if (hasEmailAddress) {
+            view.setEmailAddress(emailAddress);
         }
 
-        dto.setProjectIds(productOwner.getProjects().stream().map(Project::getId).collect(toList()));
-        return dto;
+        view.setProjectIds(projectIds);
+
+        return view;
 
     }
 }
