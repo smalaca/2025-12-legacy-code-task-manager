bb0c5d658b4350a6424d08bfb102d72debe62d2e
smalaca <sebastian.malaca@gmail.com>
2025-12-10 16:15:43 +0100

    remove usage of ProductOwner from ProductOwnerDomainModel constructor

--- src/main/java/com/smalaca/taskmanager/usermanagement/domain/productowner/ProductOwnerDomainModel.java
+++ src/main/java/com/smalaca/taskmanager/usermanagement/domain/productowner/ProductOwnerDomainModel.java
@@ -1,35 +1,25 @@
 package com.smalaca.taskmanager.usermanagement.domain.productowner;
 
-import com.smalaca.taskamanager.model.entities.ProductOwner;
-import com.smalaca.taskamanager.model.entities.Project;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
 class ProductOwnerDomainModel {
     private final Long id;
     private final String firstName;
     private final String lastName;
-    private String phonePrefix;
-    private String phoneNumber;
-    private String emailAddress;
+    private final String phonePrefix;
+    private final String phoneNumber;
+    private final String emailAddress;
     private final List<Long> projectIds;
 
-    ProductOwnerDomainModel(ProductOwner productOwner) {
-        id = productOwner.getId();
-        firstName = productOwner.getFirstName();
-        lastName = productOwner.getLastName();
-
-        if (productOwner.getPhoneNumber() != null) {
-            phonePrefix = productOwner.getPhoneNumber().getPrefix();
-            phoneNumber = productOwner.getPhoneNumber().getNumber();
-        }
-
-        if (productOwner.getEmailAddress() != null) {
-            emailAddress = productOwner.getEmailAddress().getEmailAddress();
-        }
-
-        projectIds = productOwner.getProjects().stream().map(Project::getId).collect(Collectors.toList());
+    ProductOwnerDomainModel(
+            Long id, String firstName, String lastName, List<Long> projectIds, String phoneNumber, String phonePrefix, String emailAddress) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.projectIds = projectIds;
+        this.phoneNumber = phoneNumber;
+        this.phonePrefix = phonePrefix;
+        this.emailAddress = emailAddress;
     }
 
     public ProductOwnerView asView() {
--- src/main/java/com/smalaca/taskmanager/usermanagement/domain/productowner/ProductOwnerDomainService.java
+++ src/main/java/com/smalaca/taskmanager/usermanagement/domain/productowner/ProductOwnerDomainService.java
@@ -1,9 +1,11 @@
 package com.smalaca.taskmanager.usermanagement.domain.productowner;
 
 import com.smalaca.taskamanager.model.entities.ProductOwner;
+import com.smalaca.taskamanager.model.entities.Project;
 import com.smalaca.taskamanager.repository.ProductOwnerRepository;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class ProductOwnerDomainService {
     private final ProductOwnerRepository productOwnerRepository;
@@ -14,7 +16,22 @@ public class ProductOwnerDomainService {
 
     public ProductOwnerView findById(Long id) {
         ProductOwner legacy = getProductOwnerById(id);
-        ProductOwnerDomainModel productOwner = new ProductOwnerDomainModel(legacy);
+        String phonePrefix = null;
+        String phoneNumber = null;
+        String emailAddress = null;
+        if (legacy.getPhoneNumber() != null) {
+            phonePrefix = legacy.getPhoneNumber().getPrefix();
+            phoneNumber = legacy.getPhoneNumber().getNumber();
+        }
+
+        if (legacy.getEmailAddress() != null) {
+            emailAddress = legacy.getEmailAddress().getEmailAddress();
+        }
+
+
+        ProductOwnerDomainModel productOwner = new ProductOwnerDomainModel(
+                legacy.getId(), legacy.getFirstName(), legacy.getLastName(), legacy.getProjects().stream().map(Project::getId).collect(Collectors.toList()),
+                phoneNumber, phonePrefix, emailAddress);
 
         return productOwner.asView();
     }
